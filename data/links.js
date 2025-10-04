5355b3de3441328b76d077701c63b5869b9f3ed9
clubgestionlalogia <clubgestion.lalogia@gmail.com>
2025-10-04 17:19:44 -0500

    202509041719

--- data/links.js
+++ data/links.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const enlaces = {
     "btn-templo": "templo/templo.html",
-    "btn-ofrendas": "#ofrendas",
+    "btn-ofrendas": "ofrendas/ofrendas.html",
     "btn-rituales": "https://discotecalalogia.com/eventos/#eventos",
     "btn-momentos": "https://discotecalalogia.com/#galeria",
     "btn-opinion": "https://www.google.com/maps/place/La+Logia/@6.2513021,-75.5873938,18z/data=!4m6!3m5!1s0x8e44293f52386fc1:0x64563794833ae5d0!8m2!3d6.2497539!4d-75.5885553!16s%2Fg%2F11scks2g5b?entry=ttu&g_ep=EgoyMDI1MTAwMS4wIKXMDSoASAFQAw%3D%3D",

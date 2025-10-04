84718d6825229d84ad8d2416d50aae4bbebf87b1
clubgestionlalogia <clubgestion.lalogia@gmail.com>
2025-10-04 16:12:19 -0500

    20250904_1612

--- data/templo_bienvenida.js
+++ data/templo_bienvenida.js
@@ -1,8 +1,11 @@
-document.write(
-  "Bienvenido al templo del perreo.<br><br>" +
-  "Selecciona el piso que deseas explorar en esta travesía visual.<br><br>" +
-  "En el primer piso, encontrarás:"+
-  " energía colectiva, pista general, sin distinción,<br>" +
-  "donde todos perrean con todos.<br><br>" +
-  "Y en el segundo piso, encontrarás:"+" exclusividad, palcos, zonas VIP y acceso reservado.<br><br>"
-);
+function generarBienvenida() {
+  return `
+    Bienvenido al templo del perreo.<br><br>
+    Selecciona el piso que deseas explorar en esta travesía visual.<br><br>
+    <strong>En el primer piso, encontrarás:</strong><br>
+    Energía colectiva, pista general, sin distinción,<br>
+    donde todos perrean con todos.<br><br>
+    <strong>Y en el segundo piso, encontrarás:</strong><br>
+    Exclusividad, palcos, zonas VIP y acceso reservado.<br><br>
+  `;
+}
